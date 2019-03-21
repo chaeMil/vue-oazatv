b9ce5ece18279685cf2abf49eb6641f7ccc66d38
Michal Mlejnek <chaemil72@gmail.com>
2019-03-21 11:21:27 +0100

    Add VuiKit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuikit from 'vuikit'
+import VuikitIcons from '@vuikit/icons'
+import '@vuikit/theme'
 
-Vue.config.productionTip = false
+Vue.use(Vuikit);
+Vue.use(VuikitIcons);
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
